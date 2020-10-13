538d22cdaa3b365d07a70f8d0c0abefc3ea1fb6e
Eichelbart <51883312+eichelb4rt@users.noreply.github.com>
2020-10-14 01:08:38 +0200

    Delete config.ts

--- src/config.ts
+++
@@ -1,5 +0,0 @@
-export default {
-	/*	Discord bot token, gestattet volle Berechtigung über den Bot
-		und sollte daher geheim gehalten werden */
-	botToken: "NzMyNjY3NTgzNzk4NzA2MjI4.Xw39gQ.N_UKx0XFEa3MAEJjoBMmZm1zZ_k",
-};
