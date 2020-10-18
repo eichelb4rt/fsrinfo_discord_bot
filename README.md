37f93a2a0baea5571b57114cc705d2a4d520b906
eichelb4rt <maurice.wenig@gmail.com>
2020-10-18 02:05:56 +0200

    added roles or sth like that

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 Discord Bot für den FSR Info Server
 
 Installation:
-- Benötigt Node.js >=12
+- Benötigt Node.js >=14
 - Empfohlen: Visual Studio Code mit ESLint-Erweiterung (Auf Nachfrage die Verwendung von lokalem ESLint erlauben)
 - Repository clonen
 - `npm install`
--- src/commands/Roles.ts
+++ src/commands/Roles.ts
@@ -1,14 +1,42 @@
 import Command from "../Command"
-import { Message } from "discord.js"
+import { Message, User, Role } from "discord.js"
 
 export default class Roles implements Command {
     enabled: boolean = true;
     
     condition(msg: Message): boolean {
-        return msg.content === "!role";
+        let startsWith: string = msg.content.split(" ")[0];
+        return startsWith == "!role"
     }
 
     action(msg: Message): void {
-        msg.channel.send("not implemented yet.");
+        // clean that up pls
+        let type: string = msg.content.split(" ")[1];
+        switch (type) {
+            case "add": this.add(msg); break;
+            case "remove": this.remove(msg); break;
+        }
+    }
+
+    add(msg: Message): void {
+        // adds the roles in the args
+        let args: string[] = msg.content.split(" ").slice(2);
+        let user: User = msg.author;
+        for (const rolestr of args) {
+            let role: Role | undefined = msg.guild?.roles.cache.find(role => role.name == rolestr);
+            if (role != undefined)
+                msg.guild?.member(user)?.roles.add(role);
+        }
+    }
+
+    remove(msg: Message): void {
+        // removes the roles in the args
+        let args: string[] = msg.content.split(" ").slice(2);
+        let user: User = msg.author;
+        for (const rolestr of args) {
+            let role: Role | undefined = msg.guild?.roles.cache.find(role => role.name == rolestr);
+            if (role != undefined)
+                msg.guild?.member(user)?.roles.remove(role);
+        }
     }
 }
